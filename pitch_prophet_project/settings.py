a1e47230db22daeff781dec2d45b974bbacddefb
Samuel Ekpo <basseysamuel3698@gmail.com>
2026-06-21 23:33:07 +0100

    Prep settings for deployment

--- pitch_prophet_project/settings.py
+++ pitch_prophet_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^)i+wn@5f2iq_mkpp9v4mbe!zu&)w!@h^dv#2p1q+smk*m3$e_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
