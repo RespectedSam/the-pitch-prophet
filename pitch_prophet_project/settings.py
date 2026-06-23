aa747f8b63abdbaf8a3227ef0a6af9f9e81e38d6
Samuel Ekpo <basseysamuel3698@gmail.com>
2026-06-23 01:50:38 +0100

    Connected live database and cleaned up requirements

--- pitch_prophet_project/settings.py
+++ pitch_prophet_project/settings.py
@@ -74,13 +74,16 @@ WSGI_APPLICATION = 'pitch_prophet_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
+import os
 
+# Securely handle database switching
+DATABASES = {
+    'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        conn_max_age=600
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
